f6041f18594465d736d76b7ba2485698e867f297
Vidhi  Vij <168618514+VidhiVij@users.noreply.github.com>
2026-01-21 11:43:19 +0530

    Update seedAdmin.js

--- backend/seedAdmin.js
+++ backend/seedAdmin.js
@@ -2,29 +2,26 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const Admin = require("./models/Admin");
 
-async function seedAdmin() {
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Atlas connected"))
+  .catch(err => console.error(err));
+
+async function createAdmin() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Atlas connected");
-
     const exists = await Admin.findOne({ username: "admin" });
-
     if (!exists) {
       const admin = new Admin({
         username: "admin",
         password: "admin123"
       });
-
       await admin.save();
-      console.log("✅ Admin created successfully");
+      console.log("Admin created in Atlas");
     } else {
-      console.log("ℹ️ Admin already exists");
+      console.log("Admin already exists");
     }
-  } catch (err) {
-    console.error(err);
   } finally {
     mongoose.disconnect();
   }
 }
 
-seedAdmin();
+createAdmin();
